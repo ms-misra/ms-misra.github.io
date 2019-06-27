38a4a8e9f4217259ea88ee2ea95c48addb10af39
ms-misra <52096899+ms-misra@users.noreply.github.com>
2019-06-27 22:07:03 +0530

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -4,26 +4,29 @@
  
 
 $value1=filter_input(INPUT_POST,'From_user');
-
 $value2=filter_input(INPUT_POST,'To_user');
 $value3=filter_input(INPUT_POST,'Credits');
-$value4="select CREDITS from Users where NAME=$value1";
 
+$value4="select * from Users where name='" . $value1 . "'";
+$result = mysqli_query($db, $value4);
+$row=mysqli_fetch_array($result);
 
-if($value3 <= $value4) 
+if($value3 > $row["CREDITS"])
      {
-            $query = "update Users set CREDITS=CREDITS-" . $value3 . " where name='" . $value1 . "'";
+         echo "ERROR: Credits transferred cannot be more than user's current credits ."  . "<br>";
             
-            $query2= "update Users set CREDITS=CREDITS+" . $value3 . " where name='" . $value2 . "'";
-            //mysqli_query($db,$query);
-            $sql="insert into Transfers () values ('$value1','$value2','$value3')";
-            mysqli_query($db,$sql);
-            mysqli_query($db,$query);
-            mysqli_query($db,$query2);
         }
         else 
       {
-            echo "Credits transferred cannot be more than " . $value4 . "<br>";
+            $query = "update Users set CREDITS=CREDITS-" . $value3 . " where name='" . $value1 . "'";
+            
+            $query2= "update Users set CREDITS=CREDITS+" . $value3 . " where name='" . $value2 . "'";
+           
+            $sql="insert into Transfers () values ('$value1','$value2','$value3')";
+		
+            mysqli_query($db,$sql);
+            mysqli_query($db,$query);
+            mysqli_query($db,$query2);
             
         }
     
@@ -41,11 +44,11 @@ if($value3 <= $value4)
        <nav>
                    <a href="index.php" class="active" > USERS INFO </a>
                    <a href="transaction.php"> TRANSFERS </a>
+                   
 
        </nav>
-</header>
-       <p> Transferred.</p>
-    
+       </header>
+       <p> Proceed to transfers.</p>
+    </header>
 <body>
-</html>
-
+	</html>
