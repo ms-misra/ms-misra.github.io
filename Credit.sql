4e30e9c95879a6d5a55c47c935a39c9586ac7dd3
ms-misra <52096899+ms-misra@users.noreply.github.com>
2019-06-28 12:08:09 +0530

    Update Credit.sql

--- Credit.sql
+++ Credit.sql
@@ -1,59 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jun 27, 2019 at 12:46 PM
--- Server version: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.0.25-0ubuntu0.16.04.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `Credit`
---
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `transfers`
---
-
 CREATE TABLE `Transfers` (
   `From_user` varchar(20) DEFAULT NULL,
   `To_user` varchar(20) DEFAULT NULL,
   `Credits` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `Transfers`
---
-
-
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `Users`
---
-
 CREATE TABLE `Users` (
   `NAME` varchar(20) DEFAULT NULL,
   `EMAIL` varchar(20) DEFAULT NULL,
   `CREDITS` int(11) DEFAULT '100'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
 -- Dumping data for table `Users`
---
-
 INSERT INTO `Users` (`NAME`, `EMAIL`, `CREDITS`) VALUES
 ('Ayushi','ayushi@gmail.com',100),
 ('Abhinav','abhinav@gmail.com',100),
@@ -65,7 +29,3 @@ INSERT INTO `Users` (`NAME`, `EMAIL`, `CREDITS`) VALUES
 ('Adhiraj','adhiraj@gmail.com',100),
 ('Aditya','aditya@gmail.com',100),
 ('Saloni','saloni@gmail.com',100);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
