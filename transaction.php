1e7b019ae8fa9d521721d826f52d4e36a22bb855
ms-misra <52096899+ms-misra@users.noreply.github.com>
2019-06-28 13:01:02 +0530

    Update transaction.php

--- transaction.php
+++ transaction.php
@@ -1,5 +1,4 @@
 <?php
- //$connect = mysqli_connect('localhost','id10058662_root','qwerty123456');
  $db=new mysqli('localhost','id10058662_root','qwerty123456','id10058662_credit')
  or die('Error connecting to MySQL server.');
 ?>
@@ -11,40 +10,38 @@
         <link rel="stylesheet" href="style.css">
 </head>
 <body>
-      <header>
-          <h1> CREDIT MANAGEMENT </h1>
-          <nav>
-                   <a href="index.php"> USERS INFO </a>
-                   <a href="transaction.php" class="active"> TRANSFERS </a>
+        <header>
+                   <h1> CREDIT MANAGEMENT </h1>
+                   <nav>
+                           <a href="index.php"> USERS INFO </a>
+                           <a href="transaction.php" class="active"> TRANSFERS </a>
 
-              </nav>
-      </header>
-          <p> This views all the transactions done so far. </p>
-    
-      <table>
-             <tr>
-             <th> "TRANSFERRED FROM" </th>
-             <th> "TRANSFERRED TO" </th>
-             <th> "CREDITS" </th>
-             </tr>
+                  </nav>
+        </header>
+                  <p> This views all the transactions done so far. </p>
+        <main>
+                  <table>
+                           <tr>
+                           <th> "TRANSFERRED FROM" </th>
+                           <th> "TRANSFERRED TO" </th>
+                           <th> "CREDITS" </th>
+                           </tr>
 
-       <?php
-       $query = "SELECT * FROM Transfers";
-       $result = mysqli_query($db, $query);
-       //$row = mysqli_fetch_array($result);
+                           <?php
+                                   $query = "SELECT * FROM Transfers";
+                                   $result = mysqli_query($db, $query);
 
-       while ($row = mysqli_fetch_array($result)) 
-       {
-            echo "<tr>";
-            echo "<td>" . $row["From_user"] . "</td>";
-            echo "<td>" . $row["To_user"] . "</td>";
-            echo "<td>" . $row["Credits"] . "</td>";
-            echo "</tr>";
-       }
-       
-mysqli_close($db);
-?>
-</table>
+                                   while ($row = mysqli_fetch_array($result)) 
+                                   {
+                                         echo "<tr>";
+                                         echo "<td>" . $row["From_user"] . "</td>";
+                                         echo "<td>" . $row["To_user"] . "</td>";
+                                         echo "<td>" . $row["Credits"] . "</td>";
+                                         echo "</tr>";
+                                    }
+                                    mysqli_close($db);
+			   ?>
+                   </table>
+	</main>
 </body>
 </html>
-
